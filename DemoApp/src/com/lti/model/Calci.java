9a924da0d0352acefaf2f96edb0487d095e7afdc
Anusha Shet <anushaanil44@gmail.com>
2020-12-18 16:28:33 +0530

    Made few changes in add method.

--- DemoApp/src/com/lti/model/Calci.java
+++ DemoApp/src/com/lti/model/Calci.java
@@ -1,8 +1,8 @@
 package com.lti.model;
 
 public class Calci {
-	public void divmethod(int n1, int n2) {
-		int n3 = n1/n2;
-		System.out.println("Inside a div method");
+	public void addmethod(int n1, int n2) {
+		int n3 = (n1+n2);
+		System.out.println("Inside a add method");
 	}
 }
