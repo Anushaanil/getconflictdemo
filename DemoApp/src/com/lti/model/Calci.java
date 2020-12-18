e659beda91f07367035a9526aa97c8d54f993151
Anusha Shet <anushaanil44@gmail.com>
2020-12-18 16:42:04 +0530

    Added subs method in br1

--- DemoApp/src/com/lti/model/Calci.java
+++ DemoApp/src/com/lti/model/Calci.java
@@ -5,4 +5,8 @@ public class Calci {
 		int n3 = (n1+n2);
 		System.out.println("Inside a add method");
 	}
+	public void subsmethod(int n1, int n2) {
+		int n3 = (n1-n2);
+		System.out.println("Inside a subs method");
+	}
 }
